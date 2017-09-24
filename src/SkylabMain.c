c0b7102e341b5c627623d32301298a406eeb0755
Marwil Campos <marwilcampos@gmail.com>
2017-09-24 16:36:47 -0400

    inclusion del modulo 8 en el Skylab.h
    Hay que separar en dos archivos .h el modulo 8 y el 7 (urgente)

--- src/SkylabMain.c
+++ src/SkylabMain.c
@@ -20,20 +20,40 @@ int main(int argc, char const *argv[]) {
   sem_init(&semDuranium, 0, 0);
   sem_init(&semPrometid, 0, 0);
   sem_init(&semSemprom, 0, 0);
-
+  sem_init(&calPromerium, 0, 0);
+  sem_init(&calDuranium, 0, 0);
+  sem_init(&calPrometid, 0, 0);
+  sem_init(&calDesechum, 0, 0);
+  sem_init(&calSemprom, 0, 0);
+  
   pthread_t semPromerium, semDuranium, semPrometid,semSemprom, semTransporte;
+  pthread_t calcularPromerium, calcularDuranium, calcularPrometid, calcularSemprom, calcularDesechum;
 
+	struct Parametros_Nec All_W;
+	All_W.Tipos_MP = MAX_P;
+
+	
   pthread_create(&semTransporte,NULL, transporteHaciaElLaboratorio,NULL);
   pthread_create(&semPromerium, NULL, productorPromerium, NULL);
   pthread_create(&semDuranium, NULL, productorDuranium, NULL);
   pthread_create(&semPrometid, NULL, productorPrometid, NULL);
   pthread_create(&semSemprom, NULL, productorSemprom, NULL);
+  pthread_create(&calcularPromerium, NULL,calcular_Promerium,&All_W);
+  pthread_create(&calcularDuranium, NULL,calcular_Duranium,&All_W);			
+  pthread_create(&calcularPrometid, NULL,calcular_Prometid,&All_W);
+  pthread_create(&calcularSemprom, NULL,calcular_Semprom,&All_W);
+  pthread_create(&calcularDesechum, NULL,calcular_Desechum,&All_W);
 
   pthread_join(semTransporte,NULL );
   pthread_join(semPromerium, NULL);
   pthread_join(semDuranium, NULL);
   pthread_join(semPrometid, NULL);
   pthread_join(semSemprom, NULL);
+  pthread_join(calcularPromerium, NULL);
+  pthread_join(calcularDuranium, NULL);
+  pthread_join(calcularPrometid, NULL);
+  pthread_join(calcularSemprom, NULL);
+  pthread_join(calcularDesechum, NULL);
   pthread_exit(NULL);
 
   return 0;
